34cd70b5e5cefa4bc40f4849247060aeadfdd7dc
tuktarr <68638065+tuktarr@users.noreply.github.com>
2021-04-07 16:29:30 +0900

    Update DBandlayout
    20210407

---
+++ PhoneHub/src/main/java/com/tuktarr/phonehub/model/UserEntity.java
@@ -0,0 +1,5 @@
+package com.tuktarr.phonehub.model;
+
+public class UserEntity {
+
+}
--- PhoneHub/src/main/java/com/tuktarr/phonehub/user/UserController.java
+++ PhoneHub/src/main/java/com/tuktarr/phonehub/user/UserController.java
@@ -1,5 +1,14 @@
 package com.tuktarr.phonehub.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
 
+	@GetMapping("/login")
+	public String login() {
+		return "sign/login";
+	}
+	
 }
