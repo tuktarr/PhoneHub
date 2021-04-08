7a86f7f9dd3470af681db2c00dc9ba6546a8f07b
tuktarr <68638065+tuktarr@users.noreply.github.com>
2021-04-08 15:45:17 +0900

    UserModel and layout
    20210408

--- PhoneHub/src/main/java/com/tuktarr/phonehub/model/UserEntity.java
+++ PhoneHub/src/main/java/com/tuktarr/phonehub/model/UserEntity.java
@@ -1,5 +1,68 @@
 package com.tuktarr.phonehub.model;
 
 public class UserEntity {
-
+	private int userPk;
+	private String userEmail;
+	private String userPw;
+	private String userPwRe;
+	private String nickname;
+	private String regDt;
+	private String gender;
+	private String birthday;
+	private String phone;
+	public int getUserPk() {
+		return userPk;
+	}
+	public void setUserPk(int userPk) {
+		this.userPk = userPk;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String getUserPwRe() {
+		return userPwRe;
+	}
+	public void setUserPwRe(String userPwRe) {
+		this.userPwRe = userPwRe;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getRegDt() {
+		return regDt;
+	}
+	public void setRegDt(String regDt) {
+		this.regDt = regDt;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
 }
