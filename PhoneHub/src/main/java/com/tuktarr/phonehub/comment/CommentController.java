742e281afa559c1a819b207730de241bd16647ac
tuktarr <68638065+tuktarr@users.noreply.github.com>
2021-06-10 16:28:03 +0900

    ccmt 작업 중 (삭제기능 추가)
    20210610

--- PhoneHub/src/main/java/com/tuktarr/phonehub/comment/CommentController.java
+++ PhoneHub/src/main/java/com/tuktarr/phonehub/comment/CommentController.java
@@ -4,20 +4,27 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tuktarr.phonehub.model.CommentEntity;
+import com.tuktarr.phonehub.utils.SecurityUtils;
 
 @Controller
 public class CommentController {
 
 	@Autowired
 	CommentService cService;
+	
+	@Autowired
+	SecurityUtils sUtils;
 	
 	//댓글 등록 DB로 보내기
 	@PostMapping("/comment")
@@ -33,5 +40,14 @@ public class CommentController {
 	@ResponseBody
 	public List<CommentEntity> selCmt(CommentEntity p) {
 		return cService.selCmt(p);
-	} 
+	}
+	
+	// delete ajax처리
+	@PostMapping("/del")
+	@ResponseBody
+	public int delCmt(CommentEntity p, HttpSession hs) {
+		System.out.println("cmtPk :" + p.getCmtPk());
+		System.out.println("userPk :" + p.getUserPk());
+			return cService.delCmt(p,hs);				
+	}
 }
