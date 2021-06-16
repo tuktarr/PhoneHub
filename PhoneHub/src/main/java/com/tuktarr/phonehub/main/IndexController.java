03866070a625e7c30ae4af93ea89bfc79e444f22
seungwooOh <75677605+seungwooOh@users.noreply.github.com>
2021-06-16 23:56:28 +0900

    뉴스 크롤링 시점 변경

--- PhoneHub/src/main/java/com/tuktarr/phonehub/main/IndexController.java
+++ PhoneHub/src/main/java/com/tuktarr/phonehub/main/IndexController.java
@@ -1,12 +1,22 @@
 package com.tuktarr.phonehub.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tuktarr.phonehub.news.NewsService;
 
 @Controller
 public class IndexController {
-
-	@GetMapping("/index")
-	public void index() {}
 	
+	@Autowired
+	private NewsService nService;
+
+	@RequestMapping("/")
+	public String index() {
+		nService.insNews();
+		
+		return "index";
+	}
+
 }
